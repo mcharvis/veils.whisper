000050f0dafa87ccf3ae00300ce625cd01631818
mcharvis <30698832+mcharvis@users.noreply.github.com>
2026-06-10 20:58:35 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -524,6 +524,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
+  eleventyConfig.addPassthroughCopy("src/site/ashen-codex-timeline.html");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
